43b4e5b7eb850572580001af2dcc10ac51ff039d
Frédéric Fouché <itsme@fredericfouche.fr>
2024-01-08 19:24:43 +0100

    save 2024-01-08 19:24:43

--- launch.sh
+++ launch.sh
@@ -26,7 +26,7 @@ else
     # Afficher les informations du commit
     echo "###############################################################################################################"
     echo "Date du commit : $commitDateFormatted"
-    echo "Dépôt : $accountName / $repoName" -foreground green
+    echo "Dépôt : $accountName / $repoName"
     echo "Commit et push terminé !"
     echo "###############################################################################################################"
 fi
